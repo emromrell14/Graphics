9ac4ddcc908d8fa5bbe44ff207f79a4fae084fac
Eric Romrell <eric_romrell@byu.edu>
2015-05-27 08:32:54 -0600

    Added keyboard handling functionality

--- cs355/LWJGL/StudentLWJGLController.java
+++ cs355/LWJGL/StudentLWJGLController.java
@@ -13,27 +13,17 @@ package cs355.LWJGL;
 import org.lwjgl.input.Keyboard;
 
 import static org.lwjgl.opengl.GL11.GL_COLOR_BUFFER_BIT;
-import static org.lwjgl.opengl.GL11.GL_LINES;
-import static org.lwjgl.opengl.GL11.GL_MODELVIEW;
-import static org.lwjgl.opengl.GL11.GL_PROJECTION;
-import static org.lwjgl.opengl.GL11.glBegin;
 import static org.lwjgl.opengl.GL11.glClear;
 import static org.lwjgl.opengl.GL11.glColor3f;
-import static org.lwjgl.opengl.GL11.glEnd;
 import static org.lwjgl.opengl.GL11.glLoadIdentity;
-import static org.lwjgl.opengl.GL11.glMatrixMode;
-import static org.lwjgl.opengl.GL11.glPushMatrix;
-import static org.lwjgl.opengl.GL11.glRotatef;
-import static org.lwjgl.opengl.GL11.glTranslatef;
-import static org.lwjgl.opengl.GL11.glVertex3d;
-import static org.lwjgl.opengl.GL11.glViewport;
-import static org.lwjgl.opengl.GL11.glOrtho;
-import static org.lwjgl.util.glu.GLU.gluPerspective;
 
 /**
  * @author Brennan Smith
  */
 public class StudentLWJGLController implements CS355LWJGLController {
+
+    public static final int MOVEMENT = 2;
+    public static final double ROTATION = Math.PI / 32;
 
     //This is a model of a house.
     //It has a single method that returns an iterator full of Line3Ds.
@@ -41,6 +31,8 @@ public class StudentLWJGLController implements CS355LWJGLController {
     //It should all be fairly intuitive if you look at those classes.
     //If not, I apologize.
     private WireFrame model = new HouseModel();
+    private Point3D location = new Point3D(0, 0, 0);
+    private double rotationAngle = 0;
 
     //This method is called to "resize" the viewport to match the screen.
     //When you first start, have it be in perspective mode.
@@ -62,22 +54,34 @@ public class StudentLWJGLController implements CS355LWJGLController {
     public void updateKeyboard() {
         if (Keyboard.isKeyDown(Keyboard.KEY_A)) {
             System.out.println("I should move left");
+            location.x -= MOVEMENT;
         } else if (Keyboard.isKeyDown(Keyboard.KEY_D)) {
             System.out.println("I should move right");
+            location.x += MOVEMENT;
         } else if (Keyboard.isKeyDown(Keyboard.KEY_W)) {
             System.out.println("I should move forward");
+            location.z += MOVEMENT;
         } else if (Keyboard.isKeyDown(Keyboard.KEY_S)) {
             System.out.println("I should move backward");
+            location.z -= MOVEMENT;
         } else if (Keyboard.isKeyDown(Keyboard.KEY_Q)) {
             System.out.println("I should turn left");
+            rotationAngle += ROTATION;
         } else if (Keyboard.isKeyDown(Keyboard.KEY_E)) {
             System.out.println("I should turn right");
+            rotationAngle -= ROTATION;
         } else if (Keyboard.isKeyDown(Keyboard.KEY_R)) {
             System.out.println("I should move up");
+            location.y += MOVEMENT;
         } else if (Keyboard.isKeyDown(Keyboard.KEY_F)) {
             System.out.println("I should move down");
+            location.y -= MOVEMENT;
         } else if (Keyboard.isKeyDown(Keyboard.KEY_H)) {
             System.out.println("I should return to the original 'home' position and orientation");
+            location.x = 0;
+            location.y = 0;
+            location.z = 0;
+            rotationAngle = 0;
         } else if (Keyboard.isKeyDown(Keyboard.KEY_O)) {
             System.out.println("I should switch to orthographic projection");
         } else if (Keyboard.isKeyDown(Keyboard.KEY_P)) {
@@ -92,6 +96,9 @@ public class StudentLWJGLController implements CS355LWJGLController {
         glClear(GL_COLOR_BUFFER_BIT);
 
         //Do your drawing here.
+        glColor3f(1, 1, 1);
+        glLoadIdentity();
+
     }
 
 }
