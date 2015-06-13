8cd32a6640fc1ebb13cbea6aeea76785d792c9f8
Eric Romrell <eric_romrell@byu.edu>
2015-06-12 16:34:22 -1000

    Finished all of the lab

--- cs355/Image.java
+++ cs355/Image.java
@@ -2,6 +2,9 @@ package cs355;
 
 import java.awt.image.BufferedImage;
 import java.awt.image.WritableRaster;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Created by eric on 6/9/15.
@@ -48,42 +51,12 @@ public class Image {
         this.values = values;
     }
 
-    public double get(int row, int col) {
+    public int get(int row, int col) {
         if(row < 0 || row > values.length - 1 ||
                 col < 0 || col > values[0].length - 1) {
             return 0;
         }
         return values[row][col];
-    }
-
-    public void detectEdges() {
-
-    }
-
-    public void sharpen() {
-    }
-
-    public void blurMedian() {
-
-    }
-
-    public void blurUniform() {
-        double oneNinth = 1 / 9.0;
-        double[][] mask = {
-                {oneNinth, oneNinth, oneNinth},
-                {oneNinth, oneNinth, oneNinth},
-                {oneNinth, oneNinth, oneNinth}
-        };
-        applyMask(mask);
-    }
-
-    public void changeContrast(int contrastAmountNum) {
-        for(int i = 0; i < width; i++) {
-            for(int j = 0; j < height; j++) {
-                final double value = ((double) (values[i][j] - 128)) * Math.pow((contrastAmountNum + 100) / 100.0, 4) + 128;
-                values[i][j] = normalizeValue(value, 0, 255);
-            }
-        }
     }
 
     public void changeBrightness(int brightnessAmountNum) {
@@ -95,27 +68,115 @@ public class Image {
         }
     }
 
+    public void changeContrast(int contrastAmountNum) {
+        for(int i = 0; i < width; i++) {
+            for(int j = 0; j < height; j++) {
+                final double value = ((double) (values[i][j] - 128)) * Math.pow((contrastAmountNum + 100) / 100.0, 4) + 128;
+                values[i][j] = normalizeValue(value, 0, 255);
+            }
+        }
+    }
+
+    public void blurUniform() {
+        double oneNinth = 1 / 9.0;
+        double[][] mask = {
+                {oneNinth, oneNinth, oneNinth},
+                {oneNinth, oneNinth, oneNinth},
+                {oneNinth, oneNinth, oneNinth}
+        };
+        this.values = applyMask(mask, false);
+    }
+
+    public void blurMedian() {
+        int[][] newValues = new int[width][height];
+        for(int i = 0; i < width; i++) {
+            for(int j = 0; j < height; j++) {
+                newValues[i][j] = calculateMedian(i, j);
+            }
+        }
+        this.values = newValues;
+    }
+
+    public void sharpen() {
+        double[][] mask = {
+                {0, -1 / 2.0, 0},
+                {-1 / 2.0, 6 / 2.0, -1 / 2.0},
+                {0, -1 / 2.0, 0}
+        };
+        this.values = applyMask(mask, false);
+    }
+
+    public void detectEdges() {
+        int[][] xValues = calculateXDerivative();
+        int[][] yValues = calculateYDerivative();
+
+        int[][] newValues = new int[width][height];
+        for(int i = 0; i < xValues.length; i++) {
+            for(int j = 0; j < xValues[i].length; j++) {
+                newValues[i][j] = normalizeValue(Math.round(Math.sqrt(Math.pow(xValues[i][j] / 8.0, 2) + Math.pow(yValues[i][j] / 8.0, 2))), 0, 255);
+            }
+        }
+        this.values = newValues;
+    }
+
+    private int[][] calculateXDerivative() {
+        double[][] xMask = {
+                {-1, 0, 1},
+                {-2, 0, 2},
+                {-1, 0, 1}
+        };
+        return applyMask(xMask, true);
+    }
+
+    private int[][] calculateYDerivative() {
+        double[][] yMask = {
+                {-1, -2, -1},
+                {0, 0, 0},
+                {1, 2, 1}
+        };
+        return applyMask(yMask, true);
+    }
+
     private int normalizeValue(double value, int min, int max) {
         return (int) Math.max(min, Math.min(max, value));
     }
 
-    private void applyMask(double[][] mask) {
+    private int calculateMedian(int row, int col) {
+        List<Integer> neighbors = new ArrayList<>();
+        neighbors.add(get(row - 1, col - 1));
+        neighbors.add(get(row - 1, col));
+        neighbors.add(get(row - 1, col + 1));
+        neighbors.add(get(row, col - 1));
+        neighbors.add(get(row, col));
+        neighbors.add(get(row, col + 1));
+        neighbors.add(get(row + 1, col - 1));
+        neighbors.add(get(row + 1, col));
+        neighbors.add(get(row + 1, col + 1));
+        Collections.sort(neighbors);
+        return neighbors.get(4);
+    }
+
+    public int[][] applyMask(double[][] mask, boolean useNegatives) {
         int[][] newValues = new int[width][height];
         for(int i = 0; i < width; i++) {
             for(int j = 0; j < height; j++) {
                 //Apply the mask
-                newValues[i][j] = calculateAmountAfterMask(i, j, mask);
+                if(useNegatives) {
+                    newValues[i][j] = calculateAmountAfterMask(i, j, mask);
+                } else {
+                    newValues[i][j] = normalizeValue(calculateAmountAfterMask(i, j, mask), 0, 255);
+                }
             }
         }
-        this.values = newValues;
+        return newValues;
     }
 
     private int calculateAmountAfterMask(int row, int col, double[][] mask) {
         double newValue = 0;
 
         for(int i = 0; i < mask.length; i++) {
-            for(int j = 0; j < mask[0].length; j++) {
-                newValue += get(row - 1 + i, col - 1 + j) * mask[i][j];
+            for(int j = 0; j < mask[i].length; j++) {
+                newValue += (double) get(row - 1 + i, col - 1 + j) * mask[i][j];
             }
         }
         return (int) Math.round(newValue);
--- cs355/Tests/MiscTester.java
+++ cs355/Tests/MiscTester.java
@@ -34,15 +34,15 @@ public class MiscTester {
         assertTrue(image.get(1, 0) == 58);
         assertTrue(image.get(1, 1) == 0);
 
-        image.changeContrast(100);
+        image.changeContrast(10);
         assertTrue(image.get(0, 0) == 255);
-        assertTrue(image.get(0, 1) == 244);
-        assertTrue(image.get(1, 0) == 116);
+        assertTrue(image.get(0, 1) == 119);
+        assertTrue(image.get(1, 0) == 25);
         assertTrue(image.get(1, 1) == 0);
     }
 
     @Test
-    public void imageMaskingTest() {
+    public void imageUniformBlurTest() {
         final BufferedImage bufferedImage = new BufferedImage(3, 3, BufferedImage.TYPE_BYTE_GRAY);
         bufferedImage.getRaster().setSample(0, 0, 0, 2);
         bufferedImage.getRaster().setSample(0, 1, 0, 6);
@@ -64,6 +64,118 @@ public class MiscTester {
         assertTrue(image.get(1, 2) == 4);
         assertTrue(image.get(2, 0) == 2);
         assertTrue(image.get(2, 1) == 4);
+        assertTrue(image.get(2, 2) == 3);
+
+    }
+
+    @Test
+    public void imageMedianBlurTest() {
+        final BufferedImage bufferedImage = new BufferedImage(3, 3, BufferedImage.TYPE_BYTE_GRAY);
+        bufferedImage.getRaster().setSample(0, 0, 0, 2);
+        bufferedImage.getRaster().setSample(0, 1, 0, 6);
+        bufferedImage.getRaster().setSample(0, 2, 0, 5);
+        bufferedImage.getRaster().setSample(1, 0, 0, 1);
+        bufferedImage.getRaster().setSample(1, 1, 0, 4);
+        bufferedImage.getRaster().setSample(1, 2, 0, 4);
+        bufferedImage.getRaster().setSample(2, 0, 0, 7);
+        bufferedImage.getRaster().setSample(2, 1, 0, 9);
+        bufferedImage.getRaster().setSample(2, 2, 0, 11);
+        final Image image = new Image(bufferedImage);
+
+        image.blurMedian();
+        assertTrue(image.get(0, 0) == 0);
+        assertTrue(image.get(0, 1) == 2);
+        assertTrue(image.get(0, 2) == 0);
+        assertTrue(image.get(1, 0) == 2);
+        assertTrue(image.get(1, 1) == 5);
+        assertTrue(image.get(1, 2) == 4);
+        assertTrue(image.get(2, 0) == 0);
+        assertTrue(image.get(2, 1) == 4);
+        assertTrue(image.get(2, 2) == 0);
+    }
+
+    @Test
+    public void imageSharpenTest() {
+        final BufferedImage bufferedImage = new BufferedImage(3, 3, BufferedImage.TYPE_BYTE_GRAY);
+        bufferedImage.getRaster().setSample(0, 0, 0, 2);
+        bufferedImage.getRaster().setSample(0, 1, 0, 6);
+        bufferedImage.getRaster().setSample(0, 2, 0, 5);
+        bufferedImage.getRaster().setSample(1, 0, 0, 1);
+        bufferedImage.getRaster().setSample(1, 1, 0, 4);
+        bufferedImage.getRaster().setSample(1, 2, 0, 4);
+        bufferedImage.getRaster().setSample(2, 0, 0, 7);
+        bufferedImage.getRaster().setSample(2, 1, 0, 9);
+        bufferedImage.getRaster().setSample(2, 2, 0, 11);
+        final Image image = new Image(bufferedImage);
+
+        image.sharpen();
+        assertTrue(image.get(0, 0) == 3);
+        assertTrue(image.get(0, 1) == 13);
+        assertTrue(image.get(0, 2) == 10);
+        assertTrue(image.get(1, 0) == 0);
+        assertTrue(image.get(1, 1) == 2);
+        assertTrue(image.get(1, 2) == 2);
+        assertTrue(image.get(2, 0) == 16);
+        assertTrue(image.get(2, 1) == 16);
+        assertTrue(image.get(2, 2) == 27);
+    }
+
+    @Test
+    public void imageEdgeDetectionTest() {
+        final BufferedImage bufferedImage = new BufferedImage(3, 3, BufferedImage.TYPE_BYTE_GRAY);
+        bufferedImage.getRaster().setSample(0, 0, 0, 2);
+        bufferedImage.getRaster().setSample(0, 1, 0, 6);
+        bufferedImage.getRaster().setSample(0, 2, 0, 5);
+        bufferedImage.getRaster().setSample(1, 0, 0, 1);
+        bufferedImage.getRaster().setSample(1, 1, 0, 4);
+        bufferedImage.getRaster().setSample(1, 2, 0, 4);
+        bufferedImage.getRaster().setSample(2, 0, 0, 7);
+        bufferedImage.getRaster().setSample(2, 1, 0, 9);
+        bufferedImage.getRaster().setSample(2, 2, 0, 11);
+        final Image image = new Image(bufferedImage);
+
+
+        double[][] xMask = {
+                {-1, 0, 1},
+                {-2, 0, 2},
+                {-1, 0, 1}
+        };
+        int[][] xImage = image.applyMask(xMask, true);
+        assertTrue(xImage[0][0] == 16);
+        assertTrue(xImage[0][1] == 9);
+        assertTrue(xImage[0][2] == -16);
+        assertTrue(xImage[1][0] == 23);
+        assertTrue(xImage[1][1] == 13);
+        assertTrue(xImage[1][2] == -23);
+        assertTrue(xImage[2][0] == 22);
+        assertTrue(xImage[2][1] == 11);
+        assertTrue(xImage[2][2] == -22);
+
+        double[][] yMask = {
+                {-1, -2, -1},
+                {0, 0, 0},
+                {1, 2, 1}
+        };
+        int[][] yImage = image.applyMask(yMask, true);
+        assertTrue(yImage[0][0] == 6);
+        assertTrue(yImage[0][1] == 13);
+        assertTrue(yImage[0][2] == 12);
+        assertTrue(yImage[1][0] == 13);
+        assertTrue(yImage[1][1] == 17);
+        assertTrue(yImage[1][2] == 15);
+        assertTrue(yImage[2][0] == -6);
+        assertTrue(yImage[2][1] == -13);
+        assertTrue(yImage[2][2] == -12);
+
+        image.detectEdges();
+        assertTrue(image.get(0, 0) == 2);
+        assertTrue(image.get(0, 1) == 2);
+        assertTrue(image.get(0, 2) == 3);
+        assertTrue(image.get(1, 0) == 3);
+        assertTrue(image.get(1, 1) == 3);
+        assertTrue(image.get(1, 2) == 3);
+        assertTrue(image.get(2, 0) == 3);
+        assertTrue(image.get(2, 1) == 2);
         assertTrue(image.get(2, 2) == 3);
 
     }
